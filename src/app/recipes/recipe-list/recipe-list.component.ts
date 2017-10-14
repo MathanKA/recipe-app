dc1f943e8d63f962cf633b0a7c68254bf7f8307a
MathanKA <amheretoinspire@hotmail.com>
2017-10-14 11:35:15 +0530

    cross component communication using services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from './../recipe.service';
@@ -9,14 +9,7 @@ import { RecipeService } from './../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
-
-  // recipes: Recipe[] = [
-  //   // tslint:disable-next-line:max-line-length
-  //   new Recipe('Recipe title', 'Recipe desc', 'https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/05/54cd70dad7914_-_esq-chili-recipe-0313-de.jpg'),
-  //   new Recipe('Recipe title', 'Recipe desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
-  // ];
 
 
   constructor(private recipeService: RecipeService) { }
@@ -25,7 +18,4 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,11 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+
   private recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
     new Recipe('Recipe title', 'Recipe desc', 'https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/05/54cd70dad7914_-_esq-chili-recipe-0313-de.jpg'),
