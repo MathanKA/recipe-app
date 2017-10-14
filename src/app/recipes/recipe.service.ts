4f45892d46f64701981ba131f4149c83565c8b59
MathanKA <amheretoinspire@hotmail.com>
2017-10-14 12:49:13 +0530

    adding ingredients to recipe detail page

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
@@ -8,8 +9,20 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('Recipe title', 'Recipe desc', 'https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/05/54cd70dad7914_-_esq-chili-recipe-0313-de.jpg'),
-    new Recipe('Recipe title', 'Recipe desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+    new Recipe(
+      'Recipe title',
+      'Recipe desc', 'https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/05/54cd70dad7914_-_esq-chili-recipe-0313-de.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fries', 20)
+      ]),
+    new Recipe(
+      'Recipe title',
+      'Recipe desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('meat', 3)
+      ])
   ];
 
   getRecipes() {
