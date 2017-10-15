de11640a4e0e25e3935db87941a94d46c1d7b8b0
MathanKA <amheretoinspire@hotmail.com>
2017-10-16 01:39:00 +0530

    delete each ingredient

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -50,7 +50,11 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onCancel(){
-    this.router.navigate(['../'], {relativeTo: this.route})
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
+  onDeleteIngredient(index: number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
   }
   private initForm() {
     let recipeName = '';
